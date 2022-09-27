8a3fb5685afd3a62b1aee9aca6fd96da01d17397
hardiksavaliya_crest <hardik.savaliya@crestdatasys.com>
2022-09-27 17:55:29 +0530

    modify ec2 script

--- terraform-modules/ec2.tf
+++ terraform-modules/ec2.tf
@@ -1,3 +1,30 @@
+locals {
+  user_data = <<-EOT
+  #!/bin/bash
+  touch first.txt
+  sudo su
+  apt-get update && apt install docker.io -y
+  touch second.txt
+  apt-get update && apt-get install -y apt-transport-https curl 
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  touch thrid.txt
+  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+  deb https://apt.kubernetes.io/ kubernetes-xenial main
+  EOF
+  touch forth.txt
+  apt-get update
+  apt-get install -y kubelet kubeadm kubectl
+  apt-mark hold kubelet kubeadm kubectl
+  swapoff -a
+  touch fifth.txt
+  kubeadm init --control-plane-endpoint ${aws_eip.example.public_ip}:6443 --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU --ignore-preflight-errors=Mem
+  exit
+  touch sixth.txt
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  EOT
+}
 module "ec2_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
@@ -33,20 +60,59 @@ module "key_pair" {
 }
 
 module "ec2_instance" {
-  source   = "terraform-aws-modules/ec2-instance/aws"
-  version  = "~> 3.0"
-  for_each = toset(var.ec2_name)
-  name     = "${each.key}-ec2-instance"
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  # for_each = toset(var.ec2_name)
+  name = "${var.name}-ec2-instance"
 
-  ami           = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
   key_name               = module.key_pair.key_pair_name
   vpc_security_group_ids = [module.ec2_security_group.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
+  user_data              = <<EOT
+  #!/bin/bash -xe
+  touch first.txt
+  sudo su
+  apt-get update && apt install docker.io -y
+  touch second.txt
+  apt-get update && apt-get install -y apt-transport-https curl 
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  touch thrid.txt
+  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+  deb https://apt.kubernetes.io/ kubernetes-xenial main
+  EOF
+  touch forth.txt
+  apt-get update
+  apt-get install -y kubelet kubeadm kubectl
+  apt-mark hold kubelet kubeadm kubectl
+  swapoff -a
+  touch fifth.txt
+  kubeadm init --control-plane-endpoint ${aws_eip.example.public_ip}:6443 --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU --ignore-preflight-errors=Mem
+  exit
+  touch sixth.txt
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  EOT
+  # user_data_replace_on_change = true
 }
 
 resource "local_file" "ssh_key" {
   filename = "test.pem"
-  content = module.key_pair.private_key_pem
+  content  = module.key_pair.private_key_pem
 }
- 
+
+# resource "aws_eip" "lb" {
+#   instance = module.ec2_instance.id
+#   vpc      = true
+# }
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = module.ec2_instance.id
+  allocation_id = aws_eip.example.id
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+}
--- terraform-modules/outputs.tf
+++ terraform-modules/outputs.tf
@@ -1,4 +1,8 @@
 output "ec2_complete_public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = values(module.ec2_instance)[*].public_ip
+  value       = aws_eip.example.public_ip
 }
+# output "ec2_complete_public_ip" {
+#   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+#   value       = values(module.ec2_instance)[*].public_ip
+# }
